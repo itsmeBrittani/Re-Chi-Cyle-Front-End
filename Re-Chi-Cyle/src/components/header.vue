99fae568f7560d1936e6724cca66d195ceff9d09
Brittani Savage <brittanisavage@gmail.com>
2021-04-06 11:28:31 -0500

    Vue up and running

---
+++ Re-Chi-Cyle/src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <div>
+<img alt='recycling logo with Chicago blue bar and red star' src='../assets/RCClogo.png'/>
+</div>
+    </header>
+
+</template>
+
+
+<style scoped>
+img {
+    width: 10%;
+}
+</style>
