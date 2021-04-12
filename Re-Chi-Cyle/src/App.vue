aca89b331d640b2dd0f299b58042e38300570e80
Brittani Savage <brittanisavage@gmail.com>
2021-04-11 23:44:58 -0500

    Header and Footer components complete

--- Re-Chi-Cyle/src/App.vue
+++ Re-Chi-Cyle/src/App.vue
@@ -1,12 +1,15 @@
 <template>
 <Header />
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <Landing msg="Welcome to Re.Chi.Cyle"/>
+<Footer />
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+import Landing from './components/landing.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
@@ -19,6 +22,5 @@ import Header from './components/header.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ Re-Chi-Cyle/src/components/landing.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>{{ msg }}</h1>
+    <h2> Fast Facts about Recycling in Chicago </h2>
+    <ul>
+        <li>Less than 9% of Chicago's waste is recycled - the national average hovers roughly around 32% according to the EPA</li>
+        <li>The rate of recycling decreased after Mayor Emanuel's introduction of the Blue Cart Program</li>
+        <li>Conflicts of interest with landfill-owning, private recycling haulers have been an ongoing issue contributing to the declining rate </li>
+    </ul>
+    <h2>Re.Chi.Cyle was made in an effort to:</h2>
+    <div class='landing-module-container'>
+        <div class='landing-mod' id='find'><h3>Help residents find where to recycle certain materials</h3></div>
+        <div class='landing-mod' id='learn'><h3>Educate residents on what and how to recycle</h3></div>
+        <div class='landing-mod' id='action'><h3>Encourage residents to take part in influencing the structure and efficiency of recycling in the city</h3></div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+ul{
+    width: 80%;
+    text-align: center;
+    margin: 0 auto;
+    font-size: 14px;
+    list-style-type: square;
+}
+
+.landing-module-container{
+    display: flex;
+    text-align: center;
+    width: 90%;
+    margin: 0 auto;
+}
+
+.landing-mod{
+    width: 33%;
+    height: 100px;
+    margin: 0px 5px 10px 5px;
+}
+
+#find {
+    border: 5px solid #077707;
+}
+
+#learn{
+    border: 5px solid #F6BA13;
+}
+
+#action {
+    border: 5px solid #6A040F;
+}
+
+
+</style>
